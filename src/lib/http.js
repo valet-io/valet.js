428e2722779e971f961606e515e0dd02989a062a
Ben Drucker <bvdrucker@gmail.com>
2014-01-02 10:47:59 -0500

    Request construction, sending, and response parsing

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,6 +1,16 @@
 define(function() {
 	'use strict';
 
+	// Shim Fn.proto.bind
+	Function.prototype.bind = Function.prototype.bind || function(context) {
+		var self = this;
+
+		return function() {
+			return self.apply(context, arguments);
+		};
+	};
+
+
 	function Http(attributes, callback) {
 		for (var property in attributes) {
 			if (attributes.hasOwnProperty(property)) {
@@ -11,15 +21,31 @@ define(function() {
 
 		this.client = new XMLHttpRequest();
 		this.client.timeout = 5000;
-		this.client.responseType = 'json';
-		this.client.onreadystatechange = this.callback;
+		this.client.onreadystatechange = this._onReadyStateChange.bind(this);
 		this.client.ontimeout = this._onTimeout;
 	}
 
 	Http.prototype = {
 		send: function() {
-			this.client.open('POST', this.url).send(JSON.stringify(this.data));
-		}
+			var body = JSON.stringify(this.data);
+			this.client.open('POST', this.url);
+			this.client.setRequestHeader('Content-Type','application/json;charset=UTF-8');
+			this.client.setRequestHeader('Accept', 'application/json');
+			this.client.send(body);
+		},
+		_parseResponse: function() {
+			return {
+				bodyText: this.client.responseText,
+				body: JSON.parse(this.client.responseText),
+				status: this.client.status
+			};
+		},
+		_onReadyStateChange: function() {
+			if (this.client.readyState === 4) {
+				this.response = this._parseResponse();
+			}
+		},
+		_onTimeout: function() {}
 	};
 
 	return Http;
--- test/unit/lib/http.spec.js
+++ test/unit/lib/http.spec.js
@@ -9,6 +9,10 @@ define(['lib/http'], function(Http) {
 			this.xhr.onCreate = function(xhr) {
 				requests.push(xhr);
 			};
+
+			this.request = function() {
+				return requests[0];
+			};
 		});
 
 		afterEach(function() {
@@ -17,29 +21,100 @@ define(['lib/http'], function(Http) {
 
 		describe('Construction', function() {
 
-			it('is instantiated with properties');
-			it('can take a callback');
+			before(function() {
+				this.cb = function() {};
+				this.http = new Http({
+					p1: 'v1'
+				}, this.cb);
+			});
+
+			it('is instantiated with properties', function() {
+				expect(this.http).to.have.property('p1', 'v1');
+			});
+
+			it('can take a callback', function() {
+				expect(this.http).to.have.property('callback', this.cb);
+			});
 
 		});
 
 		describe('Sending', function() {
 
-			it('sets proper JSON headers');
-			it('sends data as a JSON');
-			it('expects JSON back');
+			beforeEach(function() {
+				var data = {p1: 'v1'};
+				this.http = new Http({
+					data: data
+				});
+				this.dataString = JSON.stringify(data);
+				this.http.send();
+			});
+
+			it('sets a JSON type', function() {
+				expect(this.request().requestHeaders['Content-Type'])
+					.to.match(/^application\/json/);
+			});
+
+			it('expects JSON back', function() {
+				expect(this.request().requestHeaders.Accept)
+					.to.be('application/json');
+			});
+
+			it('sends data as a JSON string', function() {
+				expect(this.request().requestBody)
+					.to.be(this.dataString);
+			});
+
+			it('sends a POST request', function() {
+				expect(this.request().method)
+					.to.be('POST');
+			});
+
+			it('sends async requests', function() {
+				expect(this.request().async)
+					.to.be(true);
+			});
 
 		});
 
 		describe('Receiving', function() {
 
-			it('parses the response when complete');
-			it('finds JSON on the client');
+			beforeEach(function() {
+				var data = {p1: 'v1'};
+				this.http = new Http({
+					data: data
+				});
+				this.dataString = JSON.stringify(data);
+				this.http.send();
+			});
 
 			describe('Response object', function() {
 
-				it('has the response body');
-				it('has the response code');
-				it('has the response text');
+				beforeEach(function() {
+					var r = this.response = {
+						status: 200,
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({foo: 'bar'})
+					};
+
+					this.request().respond(r.status, r.headers, r.body);
+				});
+
+				it('has the raw response body', function() {
+					expect(this.http.response.bodyText)
+						.to.eql(this.response.body);
+				});
+
+				it('has the parsed response body', function() {
+					expect(this.http.response.body)
+						.to.eql(JSON.parse(this.response.body));
+				});
+
+				it('has the response code', function() {
+					expect(this.http.response.status)
+						.to.be(this.response.status);
+				});
 
 			});
 
