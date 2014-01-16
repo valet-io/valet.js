6102f780adb5406d4508027dcb09fe87f6516316
Ben Drucker <bvdrucker@gmail.com>
2014-01-16 10:29:48 -0500

    Remove overlay after each test

--- test/unit/ui-controllers/loading.spec.js
+++ test/unit/ui-controllers/loading.spec.js
@@ -7,6 +7,10 @@ define(['src/ui-controllers/loading'], function(LoadingOverlay) {
 			this.overlay = new LoadingOverlay().prepend(document.body);
 		});
 
+		afterEach(function() {
+			this.overlay.remove();
+		});
+
 		it('is an event emitter', function() {
 			expect(this.overlay).to.have.property('emit');
 		});
@@ -22,7 +26,8 @@ define(['src/ui-controllers/loading'], function(LoadingOverlay) {
 			});
 
 			it('inserts the spinner', function() {
-				expect(document.getElementById('valet-io-loading-spinner').firstChild.getAttribute('class')).to.be('spinner');
+				var container = document.getElementById('valet-io-loading-spinner');
+				expect(container.firstChild.getAttribute('class')).to.be('spinner');
 			});
 
 		});
